e555b88c0271cdc2145fc4afbe02e4dff6d6f8d7
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2018-10-15 13:43:12 -0300

    Add redux-devtools support

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,11 @@
 import { createStore, combineReducers } from 'redux';
 import { reducer } from './component/input-component/input-component-reducer';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 
 export const buildStore = () => {
-    return createStore(combineReducers({
+    const reducers = combineReducers({
         'inputComponentState': reducer
-    } as any));
+    } as any);
+
+    return createStore(reducers, devToolsEnhancer());
 };
